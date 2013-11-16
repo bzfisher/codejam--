90f750d83abf14549b0d5acf702797a6e50cb6e0
bzfisher <me@bfisher.ca>
2013-11-16 12:23:11 -0500

    Updated to pick best model

--- guess.py
+++ guess.py
@@ -1,33 +1,40 @@
-import sys, datetime, numpy
+import sys, datetime, numpy, math
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
-dataset = sys.argv[1]
-limit = int(sys.argv[2])
+dataset = sys.argv[1] #the .csv file we are reading form
+limitStart= int(sys.argv[2]) #the first data point we are looking at
+limitLength = int(sys.argv[3]) # how many subsequent datpoint we should look at
 
 # Import dataset
 total = numpy.genfromtxt(dataset, delimiter=',')
-data = total[:limit, 0:-2]
-targets = total[:limit, -1]
+data = total[limitStart:limitStart+limitLength, 0:-2]
+targets = total[limitStart:limitStart+limitLength, -1]
 
 # Train
 rbf = svm.SVC(kernel='rbf')
 knn = KNeighborsClassifier()
 rbf.fit(data, targets)
+print 'calculated rbf model'
 knn.fit(data, targets)
-
+print 'calculated knn model'
 # Predict
 error_rbf = []
 error_knn = []
+error_RbfKnn_average=[]
 (points, dimensions) = total.shape
-for index in range(limit, points):
-  values = total[index, 0:-2]
-  correct = total[index, -1]
-  output_rbf = rbf.predict(values)
-  output_knn = knn.predict(values)
-  error_rbf.append(correct - output_rbf)
-  error_knn.append(correct - output_knn)
+for index in range(limitStart+limitLength, limitStart+limitLength+16):
+	print index
+ 	values = total[index, 0:-2]
+ 	correct = total[index, -1]
+ 	output_rbf = math.pow((correct - rbf.predict(values)),2)
+ 	output_knn = math.pow((correct - knn.predict(values)),2)
+ 	error_rbf.append(output_rbf)
+ 	error_knn.append(output_knn)
+ 	error_RbfKnn_average.append((output_rbf+output_knn)/2)
 print('Mean Error RBF: ' + str(numpy.mean(error_rbf)))
 print('Mean Error KNN: ' + str(numpy.mean(error_knn)))
+print('Mean Error AVerage: ' + str(numpy.mean(error_RbfKnn_average)))
+
 
 #  print('Values: ' + str(values))
 #  print('Correct: ' + str(correct))
