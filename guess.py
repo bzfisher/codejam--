039296afcc60198af41c6fbfa12ad5d52f9bfd72
bzfisher <me@bfisher.ca>
2013-11-16 16:27:12 -0500

    added ben's files

--- guess.py
+++ guess.py
@@ -5,41 +5,45 @@ dataset = sys.argv[1] #the .csv file we are reading form
 limitStart= int(sys.argv[2]) #the first data point we are looking at
 limitLength = int(sys.argv[3]) # how many subsequent datpoint we should look at
 
+def createData(hr):
+	return total[limitStart:(limitStart+limitLength-hr), 0:-1]
+def createTargets(hr):
+	return total[(limitStart+hr):limitStart+limitLength, -1]
+
 # Import dataset
 total = numpy.genfromtxt(dataset, delimiter=',')
-data = total[limitStart:limitStart+limitLength, 0:-2]
-targets = total[limitStart:limitStart+limitLength, -1]
+with open('predictions.csv', 'w') as predictions:
+	for hour in range(1,16):
+		data = createData(hour)
+		targets = createTargets(hour)
 
-# Train
-rbf = svm.SVC(kernel='rbf')
-knn = KNeighborsClassifier()
-rbf.fit(data, targets)
-print 'calculated rbf model'
-knn.fit(data, targets)
-print 'calculated knn model'
+		# Train
+		rbf = svm.SVC(kernel='rbf')
+		knn = KNeighborsClassifier()
+		rbf.fit(data, targets)
+		knn.fit(data, targets)
+
+		#predict
+		lastDataPoint = total[limitStart+limitLength, 0:-1]
+		predict = knn.predict(lastDataPoint)
+		predictions.write(str(predict[0])+'\n')
+
 # Predict
-error_rbf = []
-error_knn = []
-error_RbfKnn_average=[]
-(points, dimensions) = total.shape
-for index in range(limitStart+limitLength, limitStart+limitLength+16):
-	print index
- 	values = total[index, 0:-2]
- 	correct = total[index, -1]
- 	output_rbf = math.pow((correct - rbf.predict(values)),2)
- 	output_knn = math.pow((correct - knn.predict(values)),2)
- 	error_rbf.append(output_rbf)
- 	error_knn.append(output_knn)
- 	error_RbfKnn_average.append((output_rbf+output_knn)/2)
-print('Mean Error RBF: ' + str(numpy.mean(error_rbf)))
-print('Mean Error KNN: ' + str(numpy.mean(error_knn)))
-print('Mean Error AVerage: ' + str(numpy.mean(error_RbfKnn_average)))
-
-
-#  print('Values: ' + str(values))
-#  print('Correct: ' + str(correct))
-#  print('RBF: ' + str(output_rbf))
-#  print('KNN: ' + str(output_knn))
+# error_rbf = []
+# error_knn = []
+# (points, dimensions) = total.shape
+# for index in range(limitStart+limitLength, limitStart+limitLength+16):
+#  	values = total[index, 0:-1]
+#  	correct = total[index+hour, -1]
+#  	output_rbf = math.pow((correct - rbf.predict(values)),2)
+#  	output_knn = math.pow((correct - knn.predict(values)),2)
+#  	error_rbf.append(output_rbf)
+#  	error_knn.append(output_knn)
+# print('Mean Error RBF: ' + str(numpy.mean(error_rbf)))
+# print('Mean Error KNN: ' + str(numpy.mean(error_knn)))
   
+
+
+
 
 
